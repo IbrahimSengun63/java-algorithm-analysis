d9c73ba74d9383afe56a76f294afa63b04810116
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2026-05-05 10:12:28 +0300

    soru14 added

---
+++ src/main/java/com/ibrahim/soru14.java
@@ -0,0 +1,25 @@
+package com.ibrahim;
+
+/*
+PROBLEM:
+
+Bir integer array veriliyor.
+
+Amaç:
+Array içindeki maksimum elemanı bulmak.
+
+Algoritmalar:
+
+1) O(N):
+   - Basit linear scan
+
+2) O(N):
+   - Simple Divide and Conquer
+   - Array ikiye bölünür
+   - Sol tarafın maksimumu bulunur
+   - Sağ tarafın maksimumu bulunur
+   - İkisinden büyük olan cevap olur
+*/
+
+import java.util.*;
+
