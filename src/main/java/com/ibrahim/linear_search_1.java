98e6a2d84625eb0798bdc4ee1b43900616d017c3
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2026-05-12 10:12:10 +0300

    linear_search_1 added

---
+++ src/main/java/com/ibrahim/linear_search_1.java
@@ -0,0 +1,31 @@
+package com.ibrahim;
+
+/*
+PROBLEM:
+
+Bir Student class'ı oluşturuluyor.
+Bu class içinde:
+- id
+- name
+- number
+
+alanları bulunuyor.
+
+
+Rastgele Student nesneleri üretmek.
+Sonra bu nesneler içinde Linear Search kullanarak arama yapmak:
+1) id'ye göre öğrenci arama
+2) name'e göre öğrenci arama
+3) number'a göre öğrenci arama
+
+Her veri boyutu için arama süreleri karşılaştırılır.
+
+Algoritma:
+
+Linear Search:
+- O(N)
+- Elemanlar baştan sona tek tek kontrol edilir
+- Aranan değer bulunursa ilgili öğrenci döndürülür
+- Bulunamazsa null döndürülür
+*/
+
