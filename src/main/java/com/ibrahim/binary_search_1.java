5af6bd372ce524279a66d25cae0c97f67de6328b
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2026-05-12 11:22:33 +0300

    binary_search_1 added

---
+++ src/main/java/com/ibrahim/binary_search_1.java
@@ -0,0 +1,30 @@
+package com.ibrahim;
+
+/*
+PROBLEM:
+
+Bir Student class'ı oluşturuluyor.
+Bu class içinde:
+- id
+- name
+- number
+
+alanları bulunuyor.
+
+Amaç:
+Rastgele Student nesneleri üretmek.
+Sonra bu öğrenciler id değerine göre sıralanır.
+Daha sonra Binary Search kullanılarak belirli bir id değerine sahip öğrenci aranır.
+
+Her veri boyutu için Binary Search süresi ölçülür.
+
+Algoritma:
+
+Binary Search:
+- O(log N)
+- Veri sıralı olmalıdır.
+- Ortadaki eleman kontrol edilir.
+- Aranan değer küçükse sol tarafa gidilir.
+- Aranan değer büyükse sağ tarafa gidilir.
+*/
+
